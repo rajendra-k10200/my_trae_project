521e5da9f4668cb2126b6bb0171953d44ce77e36
user <user@example.com>
2025-10-04 17:40:35 -0700

    Initial commit of the chatbot application.

--- llm_app.py
+++ llm_app.py
@@ -9,23 +9,23 @@ from google import genai
 client= genai.Client()
 #prompt = 'who are you?'
 
+#while(True):
+#   prompt = input('user: ')
+#   if prompt == 'exit':
+#       break
+#  response = client.models.generate_content(
+#      model='gemini-2.5-flash',
+#        contents=[prompt]
+#    )
 while(True):
-   prompt = input('user: ')
-   if prompt == 'exit':
-        break
-   response = client.models.generate_content(
+    prompt = input('user: ')
+#    if prompt == 'exit':
+#        break
+    response = client.models.generate_content(
         model='gemini-2.5-flash',
         contents=[prompt]
     )
-#while(True):
-#    prompt = input('user: ')
-##    if prompt == 'exit':
-##        break
-#    response = client.models.generate_content(
-#        model='gemini-2.5-flash',
-#        contents=[prompt]
-#    )
-   print(response.text)
+    print(response.text)
  
 
 
